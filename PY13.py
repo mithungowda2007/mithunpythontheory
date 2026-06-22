10c1bb64318bd45de93acace32695bb8f07435f4
mithungowda2007 <msmithungowda@gmail.com>
2026-06-22 15:20:36 +0530

    Add files via upload

---
+++ PY13.py
@@ -0,0 +1,15 @@
+
+def is_palindrome(s):
+    s = ''.join(filter(str.isalnum, s)).lower()
+    if len(s) <= 1:
+        return True
+    else:
+        return s[0] == s[-1] and is_palindrome(s[1:-1])
+
+
+print(f"Is 'madam' a palindrome? {is_palindrome('madam')}")
+print(
+    f"Is 'A man, a plan, a canal: Panama' a palindrome? "
+    f"{is_palindrome('A man, a plan, a canal: Panama')}"
+)
+print(f"Is 'hello' a palindrome? {is_palindrome('hello')}")
